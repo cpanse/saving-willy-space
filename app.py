cf95b1f993aaead3e446d316cf512768808b6b47
Charlotte Weil <weilcharlotte@gmail.com>
2024-10-24 13:06:59 +0200

    Add title on app

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ from streamlit_folium import folium_static
 
 visp_loc = 46.295833, 7.883333
 #m = folium.Map(location=visp_loc, zoom_start=9)
-m = folium.Map(location=visp_loc, zoom_start=9, 
-                              tiles='https://tile.opentopomap.org/{z}/{x}/{y}.png', 
+
+
+st.markdown("# :whale: :whale: Cetaceans :red[& friends] :balloon:")
+
+m = folium.Map(location=visp_loc, zoom_start=9,
+                              tiles='https://tile.opentopomap.org/{z}/{x}/{y}.png',
                               attr='<a href="https://opentopomap.org/">Open Topo Map</a>')
 
 folium_static(m)
